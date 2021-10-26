6a2fddfd6374f95c1d47f02c4ebfd1899c4b6507
justin long <justinxlong93@gmail.com>
2021-10-26 16:53:10 -0500

    initial commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = "C#";
+
+            Console.WriteLine("What is your favorite subject, C# or history?");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "C#":
+                    Console.WriteLine("You are in the right class");
+                    break;
+                case "history":
+                    Console.WriteLine("History is fun");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
